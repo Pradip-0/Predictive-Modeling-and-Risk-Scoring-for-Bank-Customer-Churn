cf0a87a0fedd9f7832266f2213744fa4e1c32074
Pradip Hazra <cristianopradip2003@gmail.com>
2026-05-29 20:35:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -299,8 +299,8 @@ if st.session_state["current_page"] == "simulator":
     #----------------------------------------------------
     bank_preprocessed= preprocessor.transform(bank)
     if st.button("Predict Churn Risk"):
-        probabilities = classifier.predict_proba(bank_preprocessed)
-        churn_probability = probabilities[0][1]
+        probability = classifier.predict_proba(bank_preprocessed)
+        risk_percentage = probabilities[0][1]
         if risk_percentage < 30:
             color = "#2ecc71"  # Soft Green
             status_label = "🟢 Low Churn Risk"
